9cb9f48443983006bd7be1a7d1614216e50c2307
Nithin virukulla <Nvirukulla@escavox.com>
2025-06-23 20:40:56 +0530

    second commit

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   Box,
   Paper,
@@ -7,151 +7,183 @@ import {
   Button,
   Link,
   InputAdornment,
-} from '@mui/material';
-import PersonIcon from '@mui/icons-material/Person';
-import LockIcon from '@mui/icons-material/Lock';
-import { useNavigate } from 'react-router-dom';
-import toast from 'react-hot-toast';
+} from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
+import LockIcon from "@mui/icons-material/Lock";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
-import logoImage from '../assets/images/logo/escavox_logo.svg';
+import logoImage from "../assets/images/logo/escavox_logo.svg";
 
-import bg1 from '../assets/images/backgrounds/avac_nobg.jpg';
-import bg2 from '../assets/images/backgrounds/background-1.jpg';
-import bg3 from '../assets/images/backgrounds/background-2.jpg';
-import bg4 from '../assets/images/backgrounds/background-3.jpg';
-import bg5 from '../assets/images/backgrounds/background-4.jpg';
-import bg6 from '../assets/images/backgrounds/background-5.jpg';
+import bg1 from "../assets/images/backgrounds/avac_nobg.jpg";
+import bg2 from "../assets/images/backgrounds/background-1.jpg";
+import bg3 from "../assets/images/backgrounds/background-2.jpg";
+import bg4 from "../assets/images/backgrounds/background-3.jpg";
+import bg5 from "../assets/images/backgrounds/background-4.jpg";
+import bg6 from "../assets/images/backgrounds/background-5.jpg";
 
 const LoginPage = () => {
   const navigate = useNavigate();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [background, setBackground] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [background, setBackground] = useState("");
 
   const backgrounds = [bg1, bg2, bg3, bg4, bg5, bg6];
 
   const handleLogin = () => {
-    if (username === 'admin' && password === 'admin123') {
-      localStorage.setItem('isAuthenticated', 'true');
-      toast.success('Login successful!');
-      navigate('/trackinsights');
+    if (username === "admin" && password === "admin123") {
+      localStorage.setItem("isAuthenticated", "true");
+      toast.success("Login successful!");
+      navigate("/trackinsights");
     } else {
-      toast.error('Invalid credentials');
+      toast.error("Invalid credentials");
     }
   };
 
   useEffect(() => {
-    if (localStorage.getItem('isAuthenticated') === 'true') {
-      navigate('/trackinsights');
+    if (localStorage.getItem("isAuthenticated") === "true") {
+      navigate("/trackinsights");
     }
-      const randomIndex = Math.floor(Math.random() * backgrounds.length);
+    const randomIndex = Math.floor(Math.random() * backgrounds.length);
     setBackground(backgrounds[randomIndex]);
   }, []);
 
   return (
     <Box
       sx={{
-        height: '100vh',
-        backgroundImage: `url(${background})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+        height: "100vh",
+        width: "100vw",
+        background: `url(${background}) center center fixed`,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundSize: "122vh",
+        backgroundRepeat: "repeat",
+        backgroundRepeat: "repeat",
       }}
     >
       <Paper
         elevation={8}
         sx={{
-          width: 350,
+          width: 300,
+          height: 510,
           padding: 4,
-          borderRadius: 2,
-          backgroundImage: 'linear-gradient(to bottom, #fff ,rgb(155, 161, 167))',
-          textAlign: 'center',
+          textAlign: "center",
+          background: "linear-gradient(to bottom, transparent 0%, #dee2e6 30%, #dee2e6 80%)",
+
         }}
       >
         <Box mb={2}>
           <img
             src={logoImage}
             alt="Escavox Logo"
-            style={{ height: '90px', marginBottom: 8 }}
+            style={{ height: "200px", marginBottom: 5 }}
           />
         </Box>
 
         <Box>
-            <Typography
-          variant="h5"
-          fontWeight="bold"
-          sx={{ color: '#2c5878', mb: 3 }}
-        >
-          WELCOME
-        </Typography>
+          <Typography variant="h3" fontWeight="bold" sx={{ color: "#3E749B", fontFamily: "Candara,Calibri,sans-serif" }}>
+            WELCOME
+          </Typography>
 
-        <TextField
-          fullWidth
-          variant="outlined"
-          placeholder="Enter your mail"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <PersonIcon sx={{ color: '#2c5878' }} />
-              </InputAdornment>
-            ),
-            sx: {
+          <TextField
+            fullWidth
+            variant="outlined"
+            placeholder="Enter your mail"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <PersonIcon sx={{ color: "#2c5878"}} />
+                </InputAdornment>
+              ),
+              sx: {
+                borderRadius: 2,
+                backgroundColor: "#E8FOFE",
+              },
+            }}
+            sx={{
+              mb: 2,
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "#3E749B",
+                  borderWidth: "2px", // Make border thicker here
+                },
+                "&:hover fieldset": {
+                  borderColor: "#3E749B",
+                  borderWidth: "2px",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "#3E749B",
+                  borderWidth: "2px",
+                },
+              },
+            }}
+          />
+
+          <TextField
+            fullWidth
+            type="password"
+            variant="outlined"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockIcon sx={{ color: "#2c5878" }} />
+                </InputAdornment>
+              ),
+              sx: {
+                borderRadius: 2,
+                backgroundColor: "#E8FOFE",
+              },
+            }}
+            sx={{
+              mb: 2,
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "#3E749B",
+                  borderWidth: "2px", // Make border thicker here
+                },
+                "&:hover fieldset": {
+                  borderColor: "#3E749B",
+                  borderWidth: "2px",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "#3E749B",
+                  borderWidth: "2px",
+                },
+              },
+            }}
+          />
+
+          <Button
+            fullWidth
+            variant="contained"
+            sx={{
+              mt: 1,
+              width: "150px",
               borderRadius: 2,
-              backgroundColor: '#f2f2f2',
-            },
-          }}
-          sx={{ mb: 2 }}
-        />
+              backgroundColor: "#2c5878",
+              fontSize: 16,
+              paddingY: 1,
+              "&:hover": {
+                backgroundColor: "#1d3f5a",
+              },
+            }}
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
 
-        <TextField
-          fullWidth
-          type="password"
-          variant="outlined"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <LockIcon sx={{ color: '#2c5878' }} />
-              </InputAdornment>
-            ),
-            sx: {
-              borderRadius: 2,
-              backgroundColor: '#f2f2f2',
-            },
-          }}
-        />
-
-        <Button
-          fullWidth
-          variant="contained"
-          sx={{
-            mt: 3,
-            borderRadius: 2,
-            backgroundColor: '#2c5878',
-            fontSize: 16,
-            paddingY: 1,
-            '&:hover': {
-              backgroundColor: '#1d3f5a',
-            },
-          }}
-          onClick={handleLogin}
-        >
-          Login
-        </Button>
-
-        <Box mt={2}>
-          <Link href="#" underline="hover" sx={{ fontSize: 13 }}>
-            Forgot password?
-          </Link>
+          <Box mt={1}>
+            <Link href="#" underline="hover" sx={{ fontSize: 13, fontFamily: "Candara, Calibri, sans-serif;", fontWeight: 900, color: "#3e749b" }}>
+              Forgot password?
+            </Link>
+          </Box>
         </Box>
-        </Box>
-        
       </Paper>
     </Box>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
 
   const handleLogout = () => {
     localStorage.removeItem('isAuthenticated');
-    toast.success('Logged out successfully');
+    // toast.success('Logged out successfully');
     handleClose();
     navigate('/login');
   };
